74903175faea9030ce8b9bcdf3dd7c3a91818665
Kyandroo <kyandevo@student.arteveldehs.be>
2023-09-17 20:13:10 +0200

    /

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -3,12 +3,12 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 import refreshTop from "/refresh-top";
-import initializeWelcome from "/animations/welcome";
-import pinElements from "/animations/pin.js";
-import pinScroll from "/animations/pin-scroll.js";
-import swipeInElements from "/animations/stagger-in";
-import fadeInElements from "/animations/fade-in";
-import mouseMove from "/animations/mouse";
+import initializeWelcome from "animations/welcome";
+import pinElements from "animations/pin.js";
+import pinScroll from "animations/pin-scroll.js";
+import swipeInElements from "animations/stagger-in";
+import fadeInElements from "animations/fade-in";
+import mouseMove from "animations/mouse";
 
 const afterIntro = () => {
   pinElements();
